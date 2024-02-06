c269369a091ccdbcbcea1ce36705cdab8dc9e3cd
Cristina <codingcreativo@gmail.com>
2024-02-06 17:52:44 +0100

    layout-bootstrap + js

---
+++ lezione8/script.js
@@ -0,0 +1,74 @@
+// ***** funzione per far apparire un popup
+// alert("hello world!");
+
+// ***** funzione per scrivere su un documento html
+// document.write("Hello World!");
+
+// ***** dichiarare una variabile
+// let nome;
+// nome = "cri";
+
+// console.log(nome);
+
+let numeroIntero = 13;
+let numeroDecimale = 13.5;
+
+console.log("Numero Intero: " + numeroIntero, "Numero Decimale: ", numeroDecimale);
+
+let frase = "Oggi è una bella giornata!";
+console.log(frase);
+
+let trovato = true;
+let campoIndirizzo = false;
+
+console.log(trovato);
+console.log(campoIndirizzo);
+
+let frutta = ["uva", "fragola", "ciliegia", 5, true];
+console.log(frutta);
+
+let valoriBooleani = [true, false, true];
+
+// operazioni aritmetiche
+
+let a = 5;
+let b = 2;
+
+let risultato = a + b;
+console.log(risultato);
+
+risultato = a - b;
+console.log(risultato);
+
+risultato = a * b;
+console.log(risultato);
+
+risultato = a / b;
+console.log(risultato);
+
+risultato = a % b;
+console.log(risultato);
+
+risultato = a ** b;
+console.log(risultato);
+
+// operatori di assegnazione combinati
+
+let x = 10;
+
+x = x + 2;
+x += 2;
+
+// prendere dati in input
+
+let nome = prompt("inserisci il tuo nome: ");
+console.log(nome);
+
+let eta = parseInt(prompt("inserici l'età"));
+console.log(eta + 2);
+
+
+
+
+
+
